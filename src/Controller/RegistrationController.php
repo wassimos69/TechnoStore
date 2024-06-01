77adea3f8658000ae2e14cf40b90498b2c64be9f
wassim <wassim.yousfi@gmail.com>
2024-06-01 17:36:27 +0200

    mise a jour pour le graphique

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -55,10 +55,10 @@ class RegistrationController extends AbstractController
             );
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_login');
+            return $this->redirectToRoute('registration/confirmation_email.html.twig');
         }
 
-        return $this->render('registration/register.html.twig', [
+        return $this->render('registration/Gochek.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
     }
@@ -74,12 +74,12 @@ class RegistrationController extends AbstractController
         } catch (VerifyEmailExceptionInterface $exception) {
             $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
 
-            return $this->redirectToRoute('app_register');
+            return $this->redirectToRoute('app_home');
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('app_home');
     }
 }
