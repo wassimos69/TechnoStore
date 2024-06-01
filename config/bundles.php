c5cdcfa04540c40b430b32a01de05599e15cdb39
wassim <wassim.yousfi@gmail.com>
2024-06-01 16:16:52 +0200

    Dans ces étapes, je prépare l'environnement de développement :
    La création de la base de données
    La migration des entités vers la base
    Les contrôleurs des différentes pages
    La page de login
    La page de réinitialisation du mot de passe
    La page de création de compte
    L'envoi de mails

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,8 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Factory\UserFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+          UserFactory::createOne(['email'=>'admin@exampel.com']);
+        $manager->flush();
+    }
+}
