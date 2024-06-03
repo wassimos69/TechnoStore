7335ba0e0fb2c6b1ed24959543f1bc19596c9eb3
wassim <wassim.yousfi@gmail.com>
2024-06-03 16:40:36 +0200

    test

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
-{
+{ 
     #[Route('/home', name: 'app_home')]
     public function index(ManagerRegistry $mr): Response
     {
